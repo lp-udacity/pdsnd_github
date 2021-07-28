14da409db8921b8d7c1686d90596d1579500ea68
Laura Pulscher <rudnikl@icloud.com>
2021-07-27 20:13:51 -0600

    Updated the first TO DO to include listing user city selection

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs and list what city the user selected. 
     city = ''
     while city not in CITY_DATA:
         city = input("please enter city name (chicago, new york city, or washington) in all lowercase:\n").lower()
